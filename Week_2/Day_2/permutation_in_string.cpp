13a9cc6c8ae79ef36bc0051bed1190411c35cc6b
lakshyaagra <lakshyaagrawal72932@gmail.com>
2026-06-09 20:30:04 +0530

    optimised 2.Permutation_in_String

--- Week_2/Day_2/permutation_in_string.cpp
+++ Week_2/Day_2/permutation_in_string.cpp
@@ -1,30 +1,29 @@
 class Solution {
-private:
-    bool isSame(vector<int>freq1,vector<int>freq2){
-        for(int i=0;i<26;i++){
-            if(freq1[i]!=freq2[i]) return false;
-        }
-        return true;
-    }
 public:
     bool checkInclusion(string s1, string s2) {
         int n=s1.size();
+        int m=s2.size();
+
+        if(n>m) return false;
+
         vector<int>freq1(26,0);
+        vector<int>freq2(26,0);
+
         for(int i=0;i<n;i++){
             freq1[s1[i]-'a']++;
         }
-        int m=s2.size();
-        for(int i=0;i<m;i++){
-            vector<int>freq2(26,0);
-            int currIdx=i;
-            int s1Index=0; 
-            //baar baar mujhe s1 ko check krna hai ki kya wo s2 mai hai ya nhi islie baar baar index 0 krunga
-            while(s1Index<n && currIdx<m){
-                freq2[s2[currIdx]-'a']++;
-                s1Index++;
-                currIdx++;
-            }
-            if(isSame(freq1,freq2)) return true;
+        // now first window of s2;
+        for(int i=0;i<n;i++){
+            freq2[s2[i]-'a']++;
+        }
+        if(freq1==freq2) return true;
+
+        // now slide the window
+        for(int i=n;i<m;i++){
+            freq2[s2[i]-'a']++;
+            freq2[s2[i-n]-'a']--;
+
+            if(freq1==freq2) return true;
         }
         return false;
     }
