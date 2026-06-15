9a2c66559627d608fdbd15de24dd6cf5abba0aa0
Lakshya Agarwal <lakshyaagrawal72932@gmail.com>
2026-06-15 13:58:02 +0530

    3. Middle_of_the_Linked_List

---
+++ Week_3/Day_1/middle_of_the_linked_list.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        if(head==NULL || head->next==NULL){
+            return head;
+        }
+        ListNode* slow=head;
+        ListNode* fast=head->next;
+        while(slow!=NULL && fast!=NULL){
+            fast=fast->next;
+            if(fast!= NULL){
+                fast=fast->next;
+            }
+            slow=slow->next;
+        }
+        return slow;
+    }
+};
