5429dd544e78e9537854a5695bbaba7d233dea8e
Lakshya Agarwal <lakshyaagrawal72932@gmail.com>
2026-06-18 19:03:52 +0530

    1. Daily_Temperatures

--- Week_3/Day_4/daily_temperatures.cpp
+++ Week_3/Day_4/daily_temperatures.cpp
@@ -4,14 +4,13 @@ public:
         int n=temperatures.size();
         vector<int>ans(n,0);
         stack<int>st;
-        for(int i=0;i<n;i++){
+        for(int i=n-1;i>=0;i--){
             int curr=temperatures[i];
-            while(!st.empty() && temperatures[st.top()]<curr){
-                ans[st.top()]=i-st.top();
+            while(!st.empty() && temperatures[st.top()]<=curr){
                 st.pop();
             }
+            if(!st.empty()) ans[i]=st.top()-i;
             st.push(i);
         }
         return ans;
     }
-};
